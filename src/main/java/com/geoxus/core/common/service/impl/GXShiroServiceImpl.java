7e0f1dda412b84d8ef544d6f8747ad4ac244e050
枫叶思源 <britton@126.com>
2020-02-24 17:10:53 +0800

    完善管理员模块

--- src/main/java/com/geoxus/core/common/service/impl/GXShiroServiceImpl.java
+++ src/main/java/com/geoxus/core/common/service/impl/GXShiroServiceImpl.java
@@ -1,11 +1,6 @@
 package com.geoxus.core.common.service.impl;
 
-import cn.hutool.core.convert.Convert;
 import cn.hutool.core.lang.Dict;
-import cn.hutool.core.lang.TypeReference;
-import cn.hutool.core.util.ArrayUtil;
-import cn.hutool.core.util.StrUtil;
-import com.geoxus.core.common.entity.GXSAdminEntity;
 import com.geoxus.core.common.service.GXSAdminHasRolesService;
 import com.geoxus.core.common.service.GXSAdminService;
 import com.geoxus.core.common.service.GXSPermissionsService;
@@ -15,8 +10,6 @@ import com.geoxus.core.common.util.GXSpringContextUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
 
 @Service
@@ -35,25 +28,24 @@ public class GXShiroServiceImpl implements GXShiroService {
      * 获取用户角色列表
      */
     public Set<String> getAdminRoles(long adminId) {
-        Set<String> set = GXSpringContextUtils.getBean(GXSAdminHasRolesService.class).getAdminRoles(adminId);
-        return set;
+        return GXSpringContextUtils.getBean(GXSAdminHasRolesService.class).getAdminRoles(adminId);
     }
 
     @Override
     public Dict getAdminById(Long adminId) {
-        GXSAdminEntity adminEntity = (GXSAdminEntity) GXSpringContextUtils.getBean(GXSAdminService.class).getById(adminId);
-        if (null != adminEntity) {
-            return Dict.parse(adminEntity);
+        final Dict dict = GXSpringContextUtils.getBean(GXSAdminService.class).getStatus(adminId);
+        if (null == dict) {
+            return Dict.create();
         }
-        return Dict.create();
+        return dict;
     }
 
     @Override
     public boolean isSuperAdmin(Dict adminData) {
-        final String primaryKey = StrUtil.toCamelCase(GXSpringContextUtils.getBean(GXSAdminService.class).getPrimaryKey());
+        final String primaryKey = GXSpringContextUtils.getBean(GXSAdminService.class).getPrimaryKey();
         if (null != adminData.getLong(primaryKey)) {
             return adminData.getLong(primaryKey).equals(GXCommonUtils.getEnvironmentValue("super.admin.id", Long.class));
         }
-        return adminData.getLong("isSuperAdmin") == 1;
+        return adminData.getLong("is_super_admin") == 1;
     }
 }
