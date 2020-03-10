b871a7aff239c459da69ebb90e1790898bb98891
枫叶思源 <britton@126.com>
2020-03-10 11:22:58 +0800

    后台管理员token超过指定时间验证失效的问题

--- src/main/java/com/geoxus/core/common/constant/GXSAdminConstants.java
+++ src/main/java/com/geoxus/core/common/constant/GXSAdminConstants.java
@@ -1,11 +1,6 @@
 package com.geoxus.core.common.constant;
 
-import com.geoxus.core.common.annotation.GXFieldCommentAnnotation;
-
 public class GXSAdminConstants {
-    @GXFieldCommentAnnotation(zh = "超级管理员ID")
-    public static final int SUPER_ADMIN_ID = 100;
-
     protected GXSAdminConstants() {
     }
 }
--- src/main/java/com/geoxus/core/common/oauth/GXTokenManager.java
+++ src/main/java/com/geoxus/core/common/oauth/GXTokenManager.java
@@ -2,11 +2,17 @@ package com.geoxus.core.common.oauth;
 
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.lang.Dict;
+import cn.hutool.crypto.SecureUtil;
 import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson.JSONException;
 import com.geoxus.core.common.annotation.GXFieldCommentAnnotation;
 import com.geoxus.core.common.util.GXAuthCodeUtils;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import lombok.extern.slf4j.Slf4j;
+
+import java.time.Duration;
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 public class GXTokenManager {
@@ -34,6 +40,12 @@ public class GXTokenManager {
     @GXFieldCommentAnnotation(zh = "ADMIN端TOKEN即将过期的刷新时间")
     private static final int ADMIN_EXPIRES_REFRESH = 24 * 60;
 
+    @GXFieldCommentAnnotation(zh = "管理员的token缓存组件")
+    private static final Cache<String, Dict> ADMIN_TOKEN_CACHE = CacheBuilder.newBuilder().maximumSize(10000).expireAfterAccess(Duration.ofMinutes(24)).build();
+
+    private GXTokenManager() {
+    }
+
     /**
      * 生成ADMIN后台登录的token
      *
@@ -45,12 +57,12 @@ public class GXTokenManager {
      *
      * @param adminId 管理员ID
      * @param param   附加信息
-     * @return
+     * @return String
      */
     public static String generateAdminToken(long adminId, Dict param) {
         param.putIfAbsent(ADMIN_ID, adminId);
-        param.putIfAbsent("expires", DateUtil.currentSeconds() + ADMIN_EXPIRES_REFRESH);
-        return GXAuthCodeUtils.authCodeEncode(JSONUtil.toJsonStr(param), KEY);
+        final long expiry = DateUtil.currentSeconds() + ADMIN_EXPIRES_REFRESH;
+        return GXAuthCodeUtils.authCodeEncode(JSONUtil.toJsonStr(param), KEY, (int) expiry);
     }
 
     /**
@@ -64,7 +76,7 @@ public class GXTokenManager {
      *
      * @param userId 前端用户的ID
      * @param param  附加信息
-     * @return
+     * @return String
      */
     public static String generateUserToken(long userId, Dict param) {
         param.putIfAbsent(USER_ID, userId);
@@ -76,7 +88,7 @@ public class GXTokenManager {
      * 注意: 由于前端用户需要保持长时间的登录信息,在生成token字符串时,不需要指定token的过期时间,过期时间时存放在s_user_token表中的
      *
      * @param source 加密TOKEN字符串
-     * @return
+     * @return Dict
      */
     public static Dict decodeUserToken(String source) {
         try {
@@ -92,23 +104,22 @@ public class GXTokenManager {
      * 注意: 由于后端用户不需要保持长时间的登录操作,所以在生成token时,为token指定了过期时间
      *
      * @param source 加密TOKEN字符串
-     * @return
+     * @return Dict
      */
     public static Dict decodeAdminToken(String source) {
         try {
-            String s = GXAuthCodeUtils.authCodeDecode(source, KEY);
-            Dict dict = JSONUtil.toBean(s, Dict.class);
-            long currentTime = DateUtil.currentSeconds();
-            if (dict.getInt("expires") < currentTime) {
-                return null;
-            } else if (dict.getInt("expires") - currentTime < ADMIN_EXPIRES_REFRESH) {
-                dict.put("refresh", true);
-                dict.set("expires", currentTime + 2880);
-            }
-            return dict;
-        } catch (JSONException exception) {
+            String cacheKey = SecureUtil.md5(source);
+            return ADMIN_TOKEN_CACHE.get(cacheKey, () -> {
+                String s = GXAuthCodeUtils.authCodeDecode(source, KEY);
+                Dict dict = JSONUtil.toBean(s, Dict.class);
+                if (dict.isEmpty()) {
+                    return null;
+                }
+                return dict;
+            });
+        } catch (JSONException | ExecutionException exception) {
             log.error(exception.getMessage());
-            return null;
         }
+        return null;
     }
 }
