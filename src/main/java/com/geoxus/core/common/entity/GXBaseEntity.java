8246f93248e68500ae451fa99858f96e6c28c0e6
枫叶思源 <britton@126.com>
2020-03-18 09:23:02 +0800

    删除冗余的字段

--- src/main/java/com/geoxus/core/common/entity/GXBaseEntity.java
+++ src/main/java/com/geoxus/core/common/entity/GXBaseEntity.java
@@ -3,7 +3,6 @@ package com.geoxus.core.common.entity;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
-import com.geoxus.core.common.annotation.GXFieldCommentAnnotation;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -17,8 +16,4 @@ public class GXBaseEntity extends Model implements Serializable {
 
     @TableField(fill = FieldFill.UPDATE)
     private Integer updatedAt;
-
-    @TableField(exist = false)
-    @GXFieldCommentAnnotation(value = "用于标识该实体是否需要被验证")
-    private boolean enableValidateEntity = true;
 }
